af8806c6b97d99358b82f051334a14f785d3bdf0
laryssaols <l173501@dac.unicamp.br>
2023-10-13 19:49:13 -0300

    mudanças no time.js

--- src/time.js
+++ src/time.js
@@ -1,12 +1,15 @@
+let startTime = 0; // Variável para armazenar o tempo de início da partida
+let pausedTime = 0; // Variável para armazenar o tempo de pausa
+let timerInterval; // Intervalo para atualizar o tempo
+
 // Função para inicializar o temporizador
 function startTimer() {
-  let startTime = Date.now();
   startTime = Date.now() - pausedTime;
-  timerInterval = setInterval(() => updateTimer(startTime), 1000); // Atualiza a cada segundo
+  timerInterval = setInterval(updateTimer, 1000); // Atualiza a cada segundo
 }
 
 // Função para atualizar o tempo de partida
-function updateTimer(startTime) {
+function updateTimer() {
   const currentTime = Date.now();
   const elapsedTime = Math.floor((currentTime - startTime) / 1000);
   const minutes = Math.floor(elapsedTime / 60);
@@ -15,16 +18,15 @@ function updateTimer(startTime) {
   timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
-//pausar tempo
-
-function pauseTimer(){
+// Função para pausar o temporizador
+function pauseTimer() {
   clearInterval(timerInterval);
   pausedTime = Date.now() - startTime;
 }
 
-function resumeTimer(){
-  startTime = Date.now()-pausedTime;
+function resumeTimer() {
+  startTime = Date.now() - pausedTime;
   startTimer();
 }
 
-
+// Outras funções e código relacionado ao temporizador
