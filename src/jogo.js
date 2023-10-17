26c451449bc2b28c6f44e53eae420bf1f859121c
gscavo <gsscavo@gmail.com>
2023-10-17 17:50:05 -0300

    RollBack on the jogo.js

--- src/jogo.js
+++ src/jogo.js
@@ -1,20 +1,20 @@
-'use strict';
-
+"use strict"
 const canvas = document.getElementById("tetris");
 const ctx = canvas.getContext("2d");
 const scoreElement = document.getElementById("score");
 const lineElement = document.getElementById("line");
 const levelElement = document.getElementById("level");
+const timerElement = document.getElementById("timer");
 
 if (!localStorage.getItem('tamanhoTabuleiro')) {
-    localStorage.setItem('tamanhoTabuleiro', '1');
+    localStorage.setItem('tamanhoTabuleiro', '1')
 }
-let pausedTime = 0;
-let debug = true;
+
+var DEBUG = false;
 
 function drawSquare(x, y, size, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(x + 1, y + 1, size - 2, size - 2);
+    ctx.fillRect(x + 1, y + 1, size - 2, size - 2)
 }
 
 class Grid {
@@ -30,7 +30,7 @@ class Grid {
     checkCollisionX(piece, direction) {
         return piece.forma.some(el => {
             const newX = el.x + direction;
-            if (newX < 0 || newX >= this.cols) {
+            if(newX < 0 || newX >= this.cols) {
                 return true;
             } else if (this.grid[el.y][newX] != null) {
                 return true;
@@ -40,7 +40,7 @@ class Grid {
 
     checkCollisionY(piece) {
         return piece.forma.some(el => {
-            if ((el.y + 1) >= this.grid.length) {
+            if ((el.y + 1) >= this.grid.length ) {
                 return true;
             } else if (this.grid[el.y + 1][el.x] != null)
                 return true;
@@ -51,25 +51,25 @@ class Grid {
         this.grid.forEach((row, y) => {
             row.forEach((col, x) => {
                 if (col != null) {
-                    drawSquare(x * this.size, y * this.size, this.size, Peca.getCor(col));
+                    drawSquare(x * this.size, y * this.size, this.size, Peca.getCor(col))
                 }
-            });
-        });
+            })
+        })
     }
 
     //Exclui a linha
     excluirLinhas(linhasCompletas) {
         if (linhasCompletas.length < 1) {
-            return false;
+            return false
         }
-
+    
         linhasCompletas.forEach(linha => {
             this.grid.splice(linha, 1);
-            this.grid.unshift(this.newRow());
+            this.grid.unshift(this.newRow())
         });
 
         return true;
-    }
+    }  
 
     newRow() {
         return new Array(this.cols).fill(null);
@@ -77,13 +77,13 @@ class Grid {
 
     inverteHorizontal() {
         this.grid = this.grid.map(row => {
-            for (let i = 0; i <= Math.ceil((row.length) / 2); i++) {
-                let aux = row[i];
-                row[i] = row[(row.length - 1) - i];
-                row[(row.length - 1) - i] = aux;
+            for (let i = 0; i < Math.floor((row.length) / 2); i++) {
+                let aux = row[i]
+                row[i] = row[(row.length - 1)-i]
+                row[(row.length - 1)-i] = aux
             }
-            return row;
-        });
+            return row
+        })
     }
 
     putPiece(piece) {
@@ -95,16 +95,16 @@ class Grid {
     }
 
     reset() {
-        this.grid = [];
+        this.grid = []
         for (let i = 0; i < this.rows; i++) {
-            let row = this.newRow();
+            let row = this.newRow()
             this.grid.push(row);
         }
     }
 
     setSize() {
         let type = localStorage.getItem("tamanhoTabuleiro");
-        if (type === '1') {
+        if ( type === '1') {
             this.rows = 20;
             this.cols = 10;
             this.size = 20;
@@ -124,19 +124,20 @@ class Grid {
         let linhasCompletas = this.grid.map((row, index) => {
             if (!row.includes(null)) {
                 if (row.includes(-1)) {
-                    Game.inverteHorizontal();
+                    Game.inverteHorizontal()
                 }
                 console.count();
                 return index;
             }
             return null;
         });
-        linhasCompletas = linhasCompletas.filter(row => row != null);
+        linhasCompletas = linhasCompletas.filter(row => row != null)
         console.warn(`Numero de linhas completadas e removidas: ${linhasCompletas.length}`);
-        this.excluirLinhas(linhasCompletas);
+        this.excluirLinhas(linhasCompletas)
         return linhasCompletas.length;
-    }
+    }  
 }
+
 
 class Game {
     static actualPiece = null;
@@ -145,69 +146,21 @@ class Game {
     static frameWait = 20;
     static grid = new Grid();
     static image = null;
+    static lastNumLinhas = 0;
     static linhas = 0;
     static nivel = 0;
     static numPecasGeradas = 0;
     static numScore = 0;
-    static state = undefined;   // Rodando (running), Pausado (paused), 
-                                // finalizado (ended), antes de rodar (undefined)
-    static numScore = 0;
-
-    static start() {
-        Game.state = 'running';
-
-        Game.generateRandomPiece();
-
-        return Game.animationId = requestAnimationFrame(Game.loop);
-    }
-
-    //Verifica se tem uma peca especial na linha
-    static temPecaEspecial(row) {
-        for (const color of row) {
-            if (color !== 'white') {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static loop() {
-        if (Game.frame < Game.frameWait) {
-            Game.frame++;
-            return Game.animationId = requestAnimationFrame(Game.loop);
-        }
-        Game.frame = 0;
-
-        Game.VerificaPreenchimentoLinhaEAtualizaDados();
-
-        Game.resetCanvas();
-
-        Game.drawBackgroundPieces();
-
-        Game.drawPiece(Game.actualPiece);
-
-        Game.movePiece({x: 0, y: 1});
-
-        true ? Game.animationId = requestAnimationFrame(Game.loop) : undefined;
-    }
-
-    static drawBackgroundPieces() {
-        Game.grid.drawGrid();
-    }
-
-    static VerificaPreenchimentoLinhaEAtualizaDados() {
-        let linhasDeletadas = Game.grid.verificaLinhasCompletas();
-
-        if (linhasDeletadas < 1) { return; }
-
-        Game.linhas += linhasDeletadas;
-
-        Game.numScore += linhasDeletadas * 10; //bonus
-
-        Game.atualizaDados();
-    }
+    // Rodando (running), Pausado (paused), 
+    // finalizado (ended), antes de rodar (undefined)
+    static state = undefined;   
+    static timer = { id: undefined, seg: 0, min: 0, }
 
     static atualizaDados() {
+        let segundos = Game.timer.seg < 10 ? `0${Game.timer.seg}` : Game.timer.seg;
+        let minutos = Game.timer.min < 10 ? `0${Game.timer.min}` : Game.timer.min;
+
+        timerElement.innerText = `${minutos}:${segundos}`
         scoreElement.innerText = Game.numScore;
         lineElement.innerText = Game.linhas;
         levelElement.innerText = Game.nivel;
@@ -228,41 +181,53 @@ class Game {
     static controlKeys(key) {
         switch (Game.state) {
             case 'running':
-                if (key === 'Escape')
+                if (key === 'Escape') {
                     Game.pause();
-                else if (key === 'ArrowUp')
+                }
+                else if (key === 'ArrowUp') {
                     Game.rotatePiece();
-                else if (key === 'ArrowLeft')
-                    Game.movePiece({x: -1, y: 0});
-                else if (key === 'ArrowRight')
-                    Game.movePiece({x: 1, y: 0});
-                else if (key === 'ArrowDown')
-                    Game.movePiece({x: 0, y: 1});
-                else if (key === 'i' && debug)
-                    Game.inverteHorizontal();
+                }
+                else if (key === 'ArrowLeft') {
+                    Game.movePiece({x: -1, y: 0})
+                }
+                else if (key === 'ArrowRight') {
+                    Game.movePiece({x: 1, y:0})
+                }
+                else if (key === 'ArrowDown') {
+                    Game.movePiece({x: 0, y:1})
+                }
+                else if (key === 'i' && DEBUG) {
+                    Game.inverteHorizontal()
+                }
                 break;
             case 'paused':
-                if (key === 'Escape' || key === 'Enter')
-                    Game.resume();
-                else if (key === 'i' && debug) {
-                    Game.inverteHorizontal();
+                if (key === 'Escape' || key === 'Enter') {
+                    Game.resume()
+                }
+                else if (key === 'i' && DEBUG) {
+                    Game.inverteHorizontal()
                 }
                 break;
             case 'ended':
-                if (key === 'Enter')
-                    Game.start();
-                else if (key === 's' && debug)
-                    Game.changeSize();
+                if (key === 'Enter') {
+                    Game.reload()
+                    Game.start()
+                }
+                else if (key === 's' && DEBUG) {
+                    Game.changeSize()
+                }
                 break;
             default:
-                if (key === 'Enter')
-                    Game.start();
-                else if (key === 's' && debug)
-                    Game.changeSize();
+                if (key === 'Enter') {
+                    Game.start()
+                }
+                else if (key === 's' && DEBUG) {
+                    Game.changeSize()
+                }
                 break;
         }
         if (key === 'r') {
-            Game.reload();
+            Game.reload()
         }
     }
 
@@ -271,15 +236,15 @@ class Game {
     }
 
     static drawPiece(piece) {
-
+        
         piece.forma.forEach(el => {
             let {x, y} = el;
-            let size = Game.grid.size;
+            let size = Game.grid.size
             let ini_x = (x * size);
             let ini_y = (y * size);
-
+    
             drawSquare(ini_x, ini_y, size, piece.cor);
-        });
+        })
     }
 
     static fixarPecaAtual() {
@@ -288,57 +253,54 @@ class Game {
                 let {x, y} = coord;
                 return {x, y};
             });
-
+            
             if (occupiedCoordinates.some((coord) => coord.y === 0)) {
-                alert('Caiu no Game Over!');
                 Game.gameOver();
                 return;
             }
-
+            
             Game.grid.putPiece(Game.actualPiece);
         }
     }
 
     static gameOver() {
-
+        
         Game.state = 'ended';
-
+        
         cancelAnimationFrame(Game.animationId);
-
-        Game.imagem('ended');
+        
+        Game.imagem('ended')
     }
 
     static generateRandomPiece() {
-        let num = Math.floor(Math.random() * 7);
-
-        num = (num != 6 ? num : -1);
-
-        Game.actualPiece = new Peca(num);
+        let num = Math.floor(Math.random() * 7)
+        
+        num = (num != 6 ? num : -1)
+        
+        Game.actualPiece = new Peca(num)
 
         Game.numPecasGeradas++;
-
-        (Game.numPecasGeradas % 30 == 0) ? Game.aumentaNivel() : undefined;
-
-        let ini_x = Math.ceil(Game.grid.cols / 2);
-
+        
+        let ini_x = Math.ceil(Game.grid.cols/2)
+        
         Game.movePiece({x: ini_x, y: 0}, true);
     }
 
     static imagem(type) {
-        let img = new Image();
+        let img = new Image()
         switch (type) {
             case 'pause':
-                img.src = '../images/paused_game.png';
+                img.src = '../images/paused_game.png'
                 break;
             case 'start':
-                img.src = '../images/start.png';
+                img.src = '../images/start.png'
                 break;
             case 'ended':
-                img.src = '../images/gameover.png';
+                img.src = '../images/gameover.png'
                 break;
         }
-        let ini_y = (canvas.height - canvas.width) / 2;
-        img.onload = () => ctx.drawImage(img, 0, ini_y, canvas.width, canvas.width);
+        let ini_y = (canvas.height - canvas.width) / 2
+        img.onload = () => ctx.drawImage(img, 0, ini_y, canvas.width, canvas.width)
     }
 
     static inverteHorizontal() {
@@ -352,22 +314,22 @@ class Game {
             return Game.animationId = requestAnimationFrame(Game.loop);
         }
         Game.frame = 0;
-
+        
         Game.verificaPreenchimentoLinhaEAtualizaDados();
-
+        
         Game.resetCanvas();
-
+        
         Game.drawBackgroundPieces();
-
+        
         Game.drawPiece(Game.actualPiece);
 
         Game.movePiece({x: 0, y: 1});
-
+        
         Game.state != 'ended' ? Game.animationId = requestAnimationFrame(Game.loop) : undefined;
     }
 
     static movePiece(coords, isNew) {
-        if (coords.y != 0) {
+        if (coords.y !=0) {
             if (Game.grid.checkCollisionY(Game.actualPiece)) {
                 Game.fixarPecaAtual();
                 Game.generateRandomPiece();
@@ -387,70 +349,76 @@ class Game {
     static start() {
         Game.state = 'running';
 
+        Game.runTimer()
+        
         Game.generateRandomPiece();
-
+        
         return Game.animationId = requestAnimationFrame(Game.loop);
     }
 
     static pause() {
 
-        pauseTimer();
-        if (Game.state != 'running') { return; }
-
+        if (Game.state != 'running') { return };
+        
         Game.state = 'paused';
-
+        
         cancelAnimationFrame(Game.animationId);
-
+        
         Game.imagem('pause');
+
+        clearInterval(Game.timer.id)
     }
 
-    static changeSize() {
-        Game.grid.setSize();
-        Game.grid.reset();
-        Game.resetCanvas();
-        Game.imagem('start');
-    }
-
-    static reload() {
+    static reload(){
         if (Game.animationId != undefined) {
             cancelAnimationFrame(Game.animationId);
             Game.animationId = undefined;
         }
 
+        clearInterval(Game.timer.id);
+        
         Game.state = undefined;
-
+        
         Game.numScore = 0;
-
+        
         Game.linhas = 0;
 
         Game.nivel = 0;
 
-        Game.atualizaDados();
+        Game.timer = {id: undefined, seg: 0, min: 0}
 
+        Game.atualizaDados()
+        
         Game.resetCanvas();
-
+        
         Game.grid.reset();
-
+        
         Game.imagem('start');
     }
 
-    static resume() {
-        resumeTimer();
-        Game.state = 'running';
-        Game.animationId = requestAnimationFrame(Game.loop);
+    static resetCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
     }
 
+    static resume() {
+        Game.state = 'running';
+
+        Game.runTimer()
+
+        Game.animationId = requestAnimationFrame(Game.loop);
+    }
+    
     static rotatePiece() {
         if (Game.state === 'running') {
             // Salva a posição atual da peça antes
             const oldForma = Game.actualPiece.forma;
-
+            
             // Tenta rotacionar a peça
             Game.actualPiece.rotacionarPeca();
-
+            
             // Verifica se a rotação é possível para determinada posição
             if (Game.grid.checkCollisionY(Game.actualPiece) || Game.grid.checkCollisionX(Game.actualPiece, 0)) {
-
+                
                 Game.actualPiece.forma = oldForma;
             }
         }
@@ -466,23 +434,44 @@ class Game {
         return false;
     }
 
+    static runTimer() {
+        Game.timer.id = setInterval(() => {
+            let segundos = Game.timer.seg < 10 ? `0${Game.timer.seg}` : Game.timer.seg;
+            let minutos = Game.timer.min < 10 ? `0${Game.timer.min}` : Game.timer.min;
+            
+            timerElement.innerText = `${minutos}:${segundos}`
+            
+            if (Game.timer.seg % 60 == 0 && Game.timer.seg > 0) {
+                Game.timer.min++;
+                Game.timer.seg = 0;
+            }
+            
+            Game.timer.seg++
+        }, 1000); 
+    }
+
     static verificaPreenchimentoLinhaEAtualizaDados() {
         let linhasDeletadas = Game.grid.verificaLinhasCompletas();
-
+        
         if (linhasDeletadas < 1) { return; }
-
+        
         Game.linhas += linhasDeletadas;
 
+        
         Game.numScore += linhasDeletadas * 10; //bonus
+        
+        if (Game.linhas > 0 && Game.linhas - Game.lastNumLinhas >= 10) { 
+            Game.aumentaNivel();
+            Game.lastNumLinhas += 10;
+        }
+        
 
-        (Game.linhas > 0 && Game.linhas % 10 == 0) ? Game.aumentaNivel() : undefined;
-
-        Game.atualizaDados();
+        Game.atualizaDados()
     }
 }
 
-Game.reload();
+Game.reload()
 
 document.addEventListener("keydown", e => {
-    Game.controlKeys(e.key);
+    Game.controlKeys(e.key)
 });
